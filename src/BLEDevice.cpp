6a6348602e42fba22a8b6a1f894935c105f7c57f
wangcoolc <1387172050@163.com>
2020-08-19 16:43:34 +0800

    Add Bluetooth initialization function

---
+++ src/BLEDevice.cpp
@@ -0,0 +1,79 @@
+#include <map>                 // Part of C++ Standard library
+#include <sstream>             // Part of C++ Standard library
+#include <iomanip>             // Part of C++ Standard library
+
+#include "BLEDevice.h"
+#include <Arduino.h>
+#include "Seeed_erpcUnified.h"
+
+
+
+/**
+ * Singletons for the BLEDevice.
+ */
+
+bool       initialized          = false;  
+BLEScan*   BLEDevice::_pBLEScan   = nullptr;
+
+
+/**
+ * @brief Retrieve the Scan object that we use for scanning.
+ * @return The scanning object reference.  This is a singleton object.  The caller should not
+ * try and release/delete it.
+ */
+/* STATIC */ BLEScan* BLEDevice::getScan() {
+	if (_pBLEScan == nullptr) {
+		_pBLEScan = new BLEScan();
+	}
+	return _pBLEScan;
+} // getScan
+
+
+/**
+ * @brief Initialize the %BLE environment.
+ * @param deviceName The device name of the device.
+ */
+/* STATIC */ void BLEDevice::init(std::string deviceName) {
+	if(!initialized){
+		initialized = true; // Set the initialization flag to ensure we are only initialized once.  
+	/*
+	 *
+	 */
+	rpc_ble_init();
+	rpc_gap_config_max_le_link_num(3);
+	rpc_le_gap_init(3);
+	le_register_app_cb(gapEventHandler);
+	rpc_ble_start();
+    Serial.printf("BLEDevice::init...................");
+	}
+	
+} // init
+
+/**
+ * @brief Handle GAP events.
+ */
+/* STATIC */ T_APP_RESULT BLEDevice::gapEventHandler(
+	  uint8_t cb_type,
+	  void *p_cb_data) {
+	T_APP_RESULT ret = APP_RESULT_SUCCESS;
+#if 0	  
+	if (BLEDevice::m_pClient != nullptr) {
+		BLEDevice::m_pClient->handleGAPEvent(event, param);
+	}
+#endif
+	if (BLEDevice::_pBLEScan != nullptr) {
+		BLEDevice::getScan()->gapCallbackDefault(cb_type,p_cb_data);
+	}
+	
+#if 0
+	if(m_bleAdvertising != nullptr) {
+		BLEDevice::getAdvertising()->handleGAPEvent(event, param);
+	}
+
+	if(m_customGapHandler != nullptr) {
+		BLEDevice::m_customGapHandler(event, param);
+	}
+#endif
+	return ret;
+} // gapEventHandler
+
---
+++ src/BLEDevice.h
@@ -0,0 +1,26 @@
+/*
+ * BLEDevice.h
+ *
+ *  Created on: Mar 16, 2017
+ *      Author: kolban
+ */
+
+#ifndef MAIN_BLEDevice_H_
+#define MAIN_BLEDevice_H_
+
+#include <map>               // Part of C++ STL
+#include <string>
+#include "BLEScan.h"
+#include "FreeRTOS.h"
+#define BTDEBUG 1
+
+class BLEDevice {
+public:
+    static BLEScan*    getScan();         // Get the scan object
+	static void        init(std::string deviceName);   // Initialize the local BLE environment.
+private:
+    static BLEScan*	   _pBLEScan;
+	static T_APP_RESULT gapEventHandler(uint8_t cb_type, void *p_cb_data);	
+};	
+
+#endif /* MAIN_BLEDevice_H_ */
