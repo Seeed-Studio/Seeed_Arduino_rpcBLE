f77d78895f13769c179977b0cb3b2a63d9a38dc7
wangcoolc <1387172050@163.com>
2020-08-19 16:55:42 +0800

    Add Bluetooth address processing function

---
+++ src/BLEAddress.h
@@ -0,0 +1,34 @@
+/*
+ * BLEAddress.h
+ *
+ *  Created on: Jul 2, 2017
+ *      Author: kolban
+ */
+
+#ifndef COMPONENTS_CPP_UTILS_BLEADDRESS_H_
+#define COMPONENTS_CPP_UTILS_BLEADDRESS_H_
+
+#include <string>
+
+/// Bluetooth address length
+#define BD_ADDR_LEN     6
+
+/// Bluetooth device address
+typedef uint8_t bd_addr_t[BD_ADDR_LEN];
+
+/**
+ * @brief A %BLE device address.
+ *
+ * Every %BLE device has a unique address which can be used to identify it and form connections.
+ */
+class BLEAddress {
+public:
+    BLEAddress(bd_addr_t address);
+	BLEAddress(std::string stringAddress);
+	std::string    toString();
+
+private:
+	bd_addr_t m_address;        
+};
+
+#endif /* COMPONENTS_CPP_UTILS_BLEADDRESS_H_ */
