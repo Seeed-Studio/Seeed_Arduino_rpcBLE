6efd36f812fd37c2835827528754258e03fb35e8
wangcoolc <1387172050@163.com>
2020-08-27 19:54:05 +0800

    Add ble remote characteristic write and read value function and notify callback function

--- src/BLERemoteCharacteristic.cpp
+++ src/BLERemoteCharacteristic.cpp
@@ -22,7 +22,7 @@ BLERemoteCharacteristic::BLERemoteCharacteristic(
 	m_uuid           = BLEUUID(m_srvcId16);
 	m_end_handle      = value_handle;
 	m_pRemoteService = pRemoteService;
-//	m_notifyCallback = nullptr;
+	m_notifyCallback = nullptr;
 	m_rawData = nullptr;
 
 	retrieveDescriptors(); // Get the descriptors for this characteristic
@@ -56,6 +56,146 @@ uint16_t BLERemoteCharacteristic::getendHandle() {
 BLERemoteService* BLERemoteCharacteristic::getRemoteService() {
 	return m_pRemoteService;
 } // getRemoteService
+
+
+/**
+ * @brief Does the characteristic support reading?
+ * @return True if the characteristic supports reading.
+ */
+bool BLERemoteCharacteristic::canRead() {
+	//return (m_charProp & ESP_GATT_CHAR_PROP_BIT_READ) != 0;
+	return true;
+} // canRead
+
+
+/**
+ * @brief Does the characteristic support notifications?
+ * @return True if the characteristic supports notifications.
+ */
+bool BLERemoteCharacteristic::canNotify() {
+	//return (m_charProp & ESP_GATT_CHAR_PROP_BIT_NOTIFY) != 0;
+	return true;
+} // canNotify
+
+
+
+
+/**
+ * @brief Read the value of the remote characteristic.
+ * @return The value of the remote characteristic.
+ */
+std::string BLERemoteCharacteristic::readValue() {
+	// Check to see that we are connected.
+	if (!getRemoteService()->getClient()->isConnected()) {
+		return std::string();
+	}
+
+	m_semaphoreReadCharEvt.take("readValue");
+
+/* 	
+	esp_err_t errRc = ::esp_ble_gattc_read_char(
+		m_pRemoteService->getClient()->getGattcIf(),
+		m_pRemoteService->getClient()->getConnId(),    // The connection ID to the BLE server
+		getHandle(),                                   // The handle of this characteristic
+		m_auth);		// Security
+		
+ */
+    client_attr_read(m_pRemoteService->getClient()->getConnId(), m_pRemoteService->getClient()->getGattcIf(),getHandle());
+	
+
+	// Block waiting for the event that indicates that the read has completed.  When it has, the std::string found
+	// in m_value will contain our data.
+	m_semaphoreReadCharEvt.wait("readValue");
+
+	return m_value;
+} // readValue
+
+
+
+/**
+ * @brief Register for notifications.
+ * @param [in] notifyCallback A callback to be invoked for a notification.  If NULL is provided then we are
+ * unregistering a notification.
+ * @return N/A.
+ */
+void BLERemoteCharacteristic::registerForNotify(notify_callback notifyCallback, bool notifications) {
+	
+	m_notifyCallback = notifyCallback;   // Save the notification callback.
+
+	m_semaphoreRegForNotifyEvt.take("registerForNotify");
+
+	if (notifyCallback != nullptr) {   // If we have a callback function, then this is a registration.
+		
+		uint8_t val[] = {0x01, 0x00};
+		if(!notifications) val[0] = 0x02;
+		BLERemoteDescriptor* desc = getDescriptor(BLEUUID((uint16_t)0x2902));
+		desc->writeValue(val, 2);
+	} // End Register
+	else {   // If we weren't passed a callback function, then this is an unregistration.		
+		uint8_t val[] = {0x00, 0x00};
+		BLERemoteDescriptor* desc = getDescriptor((uint16_t)0x2902);
+		desc->writeValue(val, 2);
+	} // End Unregister
+
+	m_semaphoreRegForNotifyEvt.wait("registerForNotify");
+} // registerForNotify
+
+
+/**
+ * @brief Get the descriptor instance with the given UUID that belongs to this characteristic.
+ * @param [in] uuid The UUID of the descriptor to find.
+ * @return The Remote descriptor (if present) or null if not present.
+ */
+BLERemoteDescriptor* BLERemoteCharacteristic::getDescriptor(BLEUUID uuid) {
+	std::string v = uuid.toString();
+	for (auto &myPair : getRemoteService()->getClient()->m_descriptorMap) {
+		if (myPair.first == v) {			
+			return myPair.second;
+		}
+	}
+	return nullptr;
+} // getDescriptor
+
+void BLERemoteCharacteristic::writeValue(uint8_t newValue, bool response) {
+	writeValue(&newValue, 1, response);
+} // writeValue
+
+/**
+ * @brief Write the new value for the characteristic.
+ * @param [in] newValue The new value to write.
+ * @param [in] response Do we expect a response?
+ * @return N/A.
+ */
+void BLERemoteCharacteristic::writeValue(std::string newValue, bool response) {
+	writeValue((uint8_t*)newValue.c_str(), strlen(newValue.c_str()), response);
+} // writeValue
+
+
+void BLERemoteCharacteristic::writeValue(uint8_t* data, size_t length, bool response) {
+
+	// Check to see that we are connected.
+	if (!getRemoteService()->getClient()->isConnected()) {
+		return;
+	}
+
+	m_semaphoreWriteCharEvt.take("writeValue");
+	// Invoke the ESP-IDF API to perform the write.
+	/* esp_err_t errRc = ::esp_ble_gattc_write_char(
+		m_pRemoteService->getClient()->getGattcIf(),
+		m_pRemoteService->getClient()->getConnId(),
+		getHandle(),
+		length,
+		data,
+		response?ESP_GATT_WRITE_TYPE_RSP:ESP_GATT_WRITE_TYPE_NO_RSP,
+        m_auth
+	); */
+
+	client_attr_write(m_pRemoteService->getClient()->getConnId(),m_pRemoteService->getClient()->getGattcIf(),GATT_WRITE_TYPE_REQ,getHandle(),length,(uint8_t *)data);
+
+	m_semaphoreWriteCharEvt.wait("writeValue");
+} // writeValue
+
+
 
 /**
  * @brief Convert a BLERemoteCharacteristic to a string representation;
@@ -105,5 +245,39 @@ void BLERemoteCharacteristic::removeDescriptors() {
 	getRemoteService()->getClient()->m_descriptorMap.clear();   // Technically not neeeded, but just to be sure.
 } // removeCharacteristics
 
+T_APP_RESULT BLERemoteCharacteristic::clientCallbackDefault(T_CLIENT_ID client_id, uint8_t conn_id, void *p_data) {
+ 
+	T_APP_RESULT result = APP_RESULT_SUCCESS;
+    T_BLE_CLIENT_CB_DATA *p_ble_client_cb_data = (T_BLE_CLIENT_CB_DATA *)p_data;
+	
+    switch (p_ble_client_cb_data->cb_type)
+    {
+    case BLE_CLIENT_CB_TYPE_DISCOVERY_STATE:
+        Serial.printf("discov_state:%d\n\r", p_ble_client_cb_data->cb_content.discov_state.state);
+        break;
+    case BLE_CLIENT_CB_TYPE_DISCOVERY_RESULT:
+    {   
+        break;	
+    }
+    case BLE_CLIENT_CB_TYPE_READ_RESULT:
+        break;
+    case BLE_CLIENT_CB_TYPE_WRITE_RESULT:
+        break;
+    case BLE_CLIENT_CB_TYPE_NOTIF_IND:{
+		if (p_ble_client_cb_data->cb_content.notif_ind.handle != getHandle()) break;
+		if (m_notifyCallback != nullptr) {
+				
+			m_notifyCallback(this,p_ble_client_cb_data->cb_content.notif_ind.p_value, p_ble_client_cb_data->cb_content.notif_ind.value_size,p_ble_client_cb_data->cb_content.notif_ind.notify);
+		} // End we have a callback function ...
+		break;
+	}
+	
+        break;
+    case BLE_CLIENT_CB_TYPE_DISCONNECT_RESULT:
+        break;
+    default:
+        break;
+    }
+}
 
 
--- src/BLERemoteCharacteristic.h
+++ src/BLERemoteCharacteristic.h
@@ -5,17 +5,35 @@
 #include "BLERemoteService.h"
 #include "BLEUUID.h"
 #include "BLEFreeRTOS.h"
+#include "BLERemoteDescriptor.h"
+#include "Seeed_erpcUnified.h"
 
 class BLERemoteService;
+class BLERemoteDescriptor;
+class BLERemoteCharacteristic;
 
-//typedef void (*notify_callback)(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify);
+typedef void (*notify_callback)(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify);
 /**
  * @brief A model of a remote %BLE characteristic.
  */
 class BLERemoteCharacteristic {
+public:
 	BLEUUID     getUUID();
     uint16_t    getHandle();
 	uint16_t     getendHandle();
+	
+	bool        canRead();
+	bool        canNotify();
+	
+	std::string readValue();
+	
+	void        writeValue(uint8_t* data, size_t length, bool response = false);
+	void        writeValue(std::string newValue, bool response = false);
+	void        writeValue(uint8_t newValue, bool response = false);
+	
+	BLERemoteDescriptor* getDescriptor(BLEUUID uuid);
+	void        registerForNotify(notify_callback _callback, bool notifications = true);
+	
 	std::string toString();
 	
 	static BLERemoteCharacteristic*     _this;
@@ -39,14 +57,21 @@ private:
 	BLERemoteService*    m_pRemoteService;
 	uint8_t 			 *m_rawData;
 	
+	std::string          m_value;
+	
 	
 	BLERemoteService* getRemoteService();
-//	notify_callback		 m_notifyCallback;
+	notify_callback	  m_notifyCallback;
     void              retrieveDescriptors();
 	void              removeDescriptors();
 	bool              m_haveDescriptor;
 	
+	BLEFreeRTOS::Semaphore m_semaphoreReadCharEvt  = BLEFreeRTOS::Semaphore("ReadCharEvt");
+	BLEFreeRTOS::Semaphore  m_semaphoreRegForNotifyEvt  = BLEFreeRTOS::Semaphore("RegForNotifyEvt");
 	BLEFreeRTOS::Semaphore m_semaphoregetdescEvt = BLEFreeRTOS::Semaphore("getDescriptor");
+	BLEFreeRTOS::Semaphore  m_semaphoreWriteCharEvt     = BLEFreeRTOS::Semaphore("WriteCharEvt");
+	
+	T_APP_RESULT   clientCallbackDefault(T_CLIENT_ID client_id, uint8_t conn_id, void *p_data);
 
 
 }; // BLERemoteCharacteristic
