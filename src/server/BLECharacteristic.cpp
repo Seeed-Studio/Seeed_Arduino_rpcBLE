65fce084097f84daa77ed7fd673a77b3f645e1a7
wangcoolc <1387172050@163.com>
2020-09-03 17:22:27 +0800

    Add ble server characteristic file

---
+++ src/server/BLECharacteristic.cpp
@@ -0,0 +1,205 @@
+/*
+ * BLECharacteristic.cpp
+ *
+ *  Created on: Jun 22, 2017
+ *      Author: kolban
+ */
+#include <sstream>
+#include <string.h>
+#include <iomanip>
+#include <stdlib.h>
+#include "BLECharacteristic.h"
+#include "BLEService.h"
+#include "BLEDevice.h"
+
+#define NULL_HANDLE (0xffff)
+
+static BLECharacteristicCallbacks defaultCallback; //null-object-pattern
+
+/**
+ * @brief Construct a characteristic
+ * @param [in] uuid - UUID for the characteristic.
+ * @param [in] properties - Properties for the characteristic.
+ */
+BLECharacteristic::BLECharacteristic(BLEUUID uuid, uint32_t properties) {
+	m_bleUUID    = uuid;
+	m_handle     = NULL_HANDLE;
+	m_properties = (uint8_t)0;
+	m_pCallbacks = &defaultCallback;
+
+	setBroadcastProperty((properties & PROPERTY_BROADCAST) != 0);
+	setReadProperty((properties & PROPERTY_READ) != 0);
+	setWriteProperty((properties & PROPERTY_WRITE) != 0);
+	setNotifyProperty((properties & PROPERTY_NOTIFY) != 0);
+	setIndicateProperty((properties & PROPERTY_INDICATE) != 0);
+	setWriteNoResponseProperty((properties & PROPERTY_WRITE_NR) != 0);
+} // BLECharacteristic
+
+
+/**
+ * @brief Set the permission to broadcast.
+ * A characteristics has properties associated with it which define what it is capable of doing.
+ * One of these is the broadcast flag.
+ * @param [in] value The flag value of the property.
+ * @return N/A
+ */
+void BLECharacteristic::setBroadcastProperty(bool value) {
+#if 0	
+	if (value) {
+		m_properties = (uint8_t)(m_properties | ESP_GATT_CHAR_PROP_BIT_BROADCAST);
+	} else {
+		m_properties = (uint8_t)(m_properties & ~ESP_GATT_CHAR_PROP_BIT_BROADCAST);
+	}
+#endif 
+} // setBroadcastProperty
+
+
+/**
+ * @brief Set the Read property value.
+ * @param [in] value Set to true if we are to allow reads.
+ */
+void BLECharacteristic::setReadProperty(bool value) {
+#if 0
+	if (value) {
+		m_properties = (esp_gatt_char_prop_t)(m_properties | ESP_GATT_CHAR_PROP_BIT_READ);
+	} else {
+		m_properties = (esp_gatt_char_prop_t)(m_properties & ~ESP_GATT_CHAR_PROP_BIT_READ);
+	}
+#endif 
+} // setReadProperty
+
+
+/**
+ * @brief Set the Write property value.
+ * @param [in] value Set to true if we are to allow writes.
+ */
+void BLECharacteristic::setWriteProperty(bool value) {
+#if 0
+	if (value) {
+		m_properties = (esp_gatt_char_prop_t)(m_properties | ESP_GATT_CHAR_PROP_BIT_WRITE);
+	} else {
+		m_properties = (esp_gatt_char_prop_t)(m_properties & ~ESP_GATT_CHAR_PROP_BIT_WRITE);
+	}
+#endif
+} // setWriteProperty
+
+
+/**
+ * @brief Set the Notify property value.
+ * @param [in] value Set to true if we are to allow notification messages.
+ */
+void BLECharacteristic::setNotifyProperty(bool value) {
+#if 0
+	if (value) {
+		m_properties = (esp_gatt_char_prop_t)(m_properties | ESP_GATT_CHAR_PROP_BIT_NOTIFY);
+	} else {
+		m_properties = (esp_gatt_char_prop_t)(m_properties & ~ESP_GATT_CHAR_PROP_BIT_NOTIFY);
+	}
+#endif
+} // setNotifyProperty
+
+
+/**
+ * @brief Set the Indicate property value.
+ * @param [in] value Set to true if we are to allow indicate messages.
+ */
+void BLECharacteristic::setIndicateProperty(bool value) {
+#if 0
+	if (value) {
+		m_properties = (esp_gatt_char_prop_t)(m_properties | ESP_GATT_CHAR_PROP_BIT_INDICATE);
+	} else {
+		m_properties = (esp_gatt_char_prop_t)(m_properties & ~ESP_GATT_CHAR_PROP_BIT_INDICATE);
+	}
+#endif
+} // setIndicateProperty
+
+
+/**
+ * @brief Set the Write No Response property value.
+ * @param [in] value Set to true if we are to allow writes with no response.
+ */
+void BLECharacteristic::setWriteNoResponseProperty(bool value) {
+#if 0
+	if (value) {
+		m_properties = (esp_gatt_char_prop_t)(m_properties | ESP_GATT_CHAR_PROP_BIT_WRITE_NR);
+	} else {
+		m_properties = (esp_gatt_char_prop_t)(m_properties & ~ESP_GATT_CHAR_PROP_BIT_WRITE_NR);
+	}
+#endif
+} // setWriteNoResponseProperty
+
+
+
+/**
+ * @brief Set the value of the characteristic from string data.
+ * We set the value of the characteristic from the bytes contained in the
+ * string.
+ * @param [in] Set the value of the characteristic.
+ * @return N/A.
+ */
+void BLECharacteristic::setValue(std::string value) {
+	setValue((uint8_t*)(value.data()), value.length());
+} // setValue
+
+
+/**
+ * @brief Set the value of the characteristic.
+ * @param [in] data The data to set for the characteristic.
+ * @param [in] length The length of the data in bytes.
+ */
+void BLECharacteristic::setValue(uint8_t* data, size_t length) {
+	m_semaphoreSetValue.take();  
+	m_value.setValue(data, length);
+	m_semaphoreSetValue.give();  
+} // setValue
+
+/**
+ * @brief Get the UUID of the characteristic.
+ * @return The UUID of the characteristic.
+ */
+BLEUUID BLECharacteristic::getUUID() {
+	return m_bleUUID;
+} // getUUID
+
+
+
+/**
+ * @brief Register a new characteristic with the ESP runtime.
+ * @param [in] pService The service with which to associate this characteristic.
+ */
+void BLECharacteristic::executeCreate(BLEService* pService) {
+
+	if (m_handle != NULL_HANDLE) {
+		return;
+	}
+
+	m_pService = pService; // Save the service to which this characteristic belongs.
+
+
+//	esp_attr_control_t control;
+//	control.auto_rsp = ESP_GATT_RSP_BY_APP;
+
+	m_semaphoreCreateEvt.take("executeCreate");
+#if 0
+	esp_err_t errRc = ::esp_ble_gatts_add_char(
+		m_pService->getHandle(),
+		getUUID().getNative(),
+		static_cast<esp_gatt_perm_t>(m_permissions),
+		getProperties(),
+		nullptr,
+		&control); // Whether to auto respond or not.
+
+	if (errRc != ESP_OK) {
+		log_e("<< esp_ble_gatts_add_char: rc=%d %s", errRc, GeneralUtils::errorToString(errRc));
+		return;
+	}
+#endif
+	m_semaphoreCreateEvt.wait("executeCreate");
+
+	BLEDescriptor* pDescriptor = m_descriptorMap.getFirst();
+	while (pDescriptor != nullptr) {
+		pDescriptor->executeCreate(this);
+		pDescriptor = m_descriptorMap.getNext();
+	} // End while
+
+} // executeCreate
---
+++ src/server/BLECharacteristic.h
@@ -0,0 +1,113 @@
+/*
+ * BLECharacteristic.h
+ *
+ *  Created on: Jun 22, 2017
+ *      Author: kolban
+ */
+
+#ifndef COMPONENTS_CPP_UTILS_BLECHARACTERISTIC_H_
+#define COMPONENTS_CPP_UTILS_BLECHARACTERISTIC_H_
+#include <string>
+#include <map>
+#include "BLEUUID.h"
+#include "BLEDescriptor.h"
+#include "BLEValue.h"
+#include "BLEFreeRTOS.h"
+
+class BLEService;
+class BLEDescriptor;
+class BLECharacteristicCallbacks;
+
+/**
+ * @brief A management structure for %BLE descriptors.
+ */
+class BLEDescriptorMap {
+public:
+    void setByUUID(BLEUUID uuid, BLEDescriptor* pDescriptor);
+	BLEDescriptor* getByUUID(BLEUUID uuid);
+	BLEDescriptor* getFirst();
+	BLEDescriptor* getNext();
+	std::string	toString();
+
+
+private:
+	std::map<BLEDescriptor*, std::string> m_uuidMap;
+    std::map<BLEDescriptor*, std::string>::iterator m_iterator;
+};
+
+
+/**
+ * @brief The model of a %BLE Characteristic.
+ *
+ * A BLE Characteristic is an identified value container that manages a value. It is exposed by a BLE server and
+ * can be read and written to by a %BLE client.
+ */
+class BLECharacteristic {
+public:
+    BLECharacteristic(BLEUUID uuid, uint32_t properties = 0);
+	void setBroadcastProperty(bool value);
+	void setReadProperty(bool value);
+	void setWriteProperty(bool value);
+	void setNotifyProperty(bool value);
+	void setIndicateProperty(bool value);
+	void setWriteNoResponseProperty(bool value);
+
+	void setValue(std::string value);
+	void setValue(uint8_t* data, size_t size);
+
+	BLEUUID        getUUID();
+
+
+
+	static const uint32_t PROPERTY_READ      = 1<<0;
+	static const uint32_t PROPERTY_WRITE     = 1<<1;
+	static const uint32_t PROPERTY_NOTIFY    = 1<<2;
+	static const uint32_t PROPERTY_BROADCAST = 1<<3;
+	static const uint32_t PROPERTY_INDICATE  = 1<<4;
+	static const uint32_t PROPERTY_WRITE_NR  = 1<<5;
+
+private:
+    friend class BLEServer;
+	friend class BLEService;
+	friend class BLECharacteristicMap;
+	friend class BLEDescriptor;
+
+    BLEUUID                     m_bleUUID;
+	uint16_t                    m_handle;
+    uint8_t                     m_properties;
+    BLECharacteristicCallbacks* m_pCallbacks;
+
+	BLEValue                    m_value;
+	BLEService*                 m_pService;
+	BLEDescriptorMap            m_descriptorMap;
+
+    void                 executeCreate(BLEService* pService);
+	BLEFreeRTOS::Semaphore m_semaphoreCreateEvt = BLEFreeRTOS::Semaphore("CreateEvt");
+	BLEFreeRTOS::Semaphore m_semaphoreSetValue  = BLEFreeRTOS::Semaphore("SetValue"); 
+
+
+}; // BLECharacteristic
+
+
+/**
+ * @brief Callbacks that can be associated with a %BLE characteristic to inform of events.
+ *
+ * When a server application creates a %BLE characteristic, we may wish to be informed when there is either
+ * a read or write request to the characteristic's value. An application can register a
+ * sub-classed instance of this class and will be notified when such an event happens.
+ */
+class BLECharacteristicCallbacks {
+public:
+	typedef enum {
+		SUCCESS_INDICATE,
+		SUCCESS_NOTIFY,
+		ERROR_INDICATE_DISABLED,
+		ERROR_NOTIFY_DISABLED,
+		ERROR_GATT,
+		ERROR_NO_CLIENT,
+		ERROR_INDICATE_TIMEOUT,
+		ERROR_INDICATE_FAILURE
+	}Status;
+
+};
+#endif /* COMPONENTS_CPP_UTILS_BLECHARACTERISTIC_H_ */
