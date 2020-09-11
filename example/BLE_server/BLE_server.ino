383d0e8a7d90cebceaff1ceaa6b1eaf373ab6642
wangcoolc <1387172050@163.com>
2020-09-11 15:32:22 +0800

    Modified ble server demo process

--- example/BLE_server/BLE_server.ino
+++ example/BLE_server/BLE_server.ino
@@ -5,17 +5,36 @@
 */
 
 #include <BLEDevice.h>
-#include <BLEUtils.h>
 #include <BLEServer.h>
+#include "Seeed_erpcUnified.h"
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SERVICE_UUID        "180f"
+#define CHARACTERISTIC_UUID "2a19"
+#define DESCRIPTOR_UUID     "4545"
+
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+
+      if (rxValue.length() > 0) {
+        Serial.println("*********");
+        Serial.print("Received Value: ");
+        for (int i = 0; i < rxValue.length(); i++)
+          Serial.print(rxValue[i]);
+
+        Serial.println();
+        Serial.println("*********");
+      }
+    }
+};
 
 void setup() {
   Serial.begin(115200);
+  while(!Serial){};
   Serial.println("Starting BLE work!");
 
   BLEDevice::init("Long name works now");
@@ -26,9 +45,17 @@ void setup() {
                                          BLECharacteristic::PROPERTY_READ |
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
-
+  pCharacteristic->setAccessPermissions(GATT_PERM_READ | GATT_PERM_WRITE);
+  BLEDescriptor *pDescriptor = pCharacteristic->createDescriptor(
+                                         DESCRIPTOR_UUID,
+                                          ATTRIB_FLAG_VOID | ATTRIB_FLAG_ASCII_Z,
+                                         GATT_PERM_READ | GATT_PERM_WRITE,
+                                         2
+                                         );
   pCharacteristic->setValue("Hello World says Neil");
+  pCharacteristic->setCallbacks(new MyCallbacks());
   pService->start();
+
   // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
@@ -39,7 +66,8 @@ void setup() {
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
-void loop() {
+void loop() { 
   // put your main code here, to run repeatedly:
+  Serial.println("1");
   delay(2000);
 }
